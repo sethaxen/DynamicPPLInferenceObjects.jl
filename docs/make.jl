0de57514fdc43ceaa412174f5002783f5437a6ec
Seth Axen <seth@sethaxen.com>
2022-11-24 21:09:05 +0100

    Reset user to personal account

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,25 @@
 using DynamicPPLInferenceObjects
 using Documenter
 
-DocMeta.setdocmeta!(DynamicPPLInferenceObjects, :DocTestSetup, :(using DynamicPPLInferenceObjects); recursive=true)
+DocMeta.setdocmeta!(
+    DynamicPPLInferenceObjects,
+    :DocTestSetup,
+    :(using DynamicPPLInferenceObjects);
+    recursive=true,
+)
 
 makedocs(;
     modules=[DynamicPPLInferenceObjects],
     authors="Seth Axen <seth@sethaxen.com> and contributors",
-    repo="https://github.com/TuringLang/DynamicPPLInferenceObjects.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/sethaxen/DynamicPPLInferenceObjects.jl/blob/{commit}{path}#{line}",
     sitename="DynamicPPLInferenceObjects.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://TuringLang.github.io/DynamicPPLInferenceObjects.jl",
+        canonical="https://sethaxen.github.io/DynamicPPLInferenceObjects.jl",
         edit_link="main",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages=["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/TuringLang/DynamicPPLInferenceObjects.jl",
-    devbranch="main",
-)
+deploydocs(; repo="github.com/sethaxen/DynamicPPLInferenceObjects.jl", devbranch="main")
